6224d3b90e0bb8d47da9aa603179e0142106d67f
soyoon26 <barcardi26@gmail.com>
2024-06-16 22:45:13 +0900

    feat: add link

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./ProductList.module.css";
+import Link from "next/link";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
@@ -20,16 +21,18 @@ function ProductList() {
           products.map((product) => {
             return (
               <li key={product.id} className={styles.item}>
-                <div>
-                  <Image
-                    src={product.imageUrl}
-                    width={300}
-                    height={250}
-                    alt={product.name}
-                  ></Image>
-                </div>
-                <div>{product.name}</div>
-                <div>{product.price}</div>
+                <Link href={`/products/${product.id}`}>
+                  <div>
+                    <Image
+                      src={product.imageUrl}
+                      width={300}
+                      height={250}
+                      alt={product.name}
+                    ></Image>
+                  </div>
+                  <div>{product.name}</div>
+                  <div>{product.price}</div>
+                </Link>
               </li>
             );
           })}
