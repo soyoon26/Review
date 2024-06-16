a59ded9b0c2b2eb1a77d34e55e078665b12440c2
soyoon26 <barcardi26@gmail.com>
2024-06-16 19:05:12 +0900

    feat: add img

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 function ProductList() {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:4000/products").then((response) => {
       setProducts(response.data);
@@ -17,7 +18,20 @@ function ProductList() {
       <ul>
         {products &&
           products.map((product) => {
-            return <li key={product.id}>{product.name}</li>;
+            return (
+              <li key={product.id}>
+                <div>
+                  <Image
+                    src={product.imageUrl}
+                    width={300}
+                    height={250}
+                    alt={product.name}
+                  ></Image>
+                </div>
+                <div>{product.name}</div>
+                <div>{product.price}</div>
+              </li>
+            );
           })}
         {/* key를 적어줘야 함 */}
       </ul>
