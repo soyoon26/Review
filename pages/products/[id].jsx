89b4f485f7fc0326f7f1efc299048d445357ad17
soyoon26 <barcardi26@gmail.com>
2024-06-16 22:22:49 +0900

    feat: add detail

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,5 +1,23 @@
+import ProductHeader from "@/components/ProductHeader";
+import axios from "axios";
 import React from "react";
 
-export default function ProductInfoPage() {
-  return <div>ProductInfoPage</div>;
+export default function ProductInfoPage({ message, productInfo }) {
+  return (
+    <div>
+      <ProductHeader title="상품 상세 정보 페이지" />
+      ProductInfoPage : {message}
+      <p>{productInfo.name}</p>
+    </div>
+  );
+}
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const response = await axios.get(`http://localhost:4000/products/${id}`);
+  return {
+    props: {
+      message: "서버에서 보내준 메세지",
+      productInfo: response.data,
+    },
+  };
 }
