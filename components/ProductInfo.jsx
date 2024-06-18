d6f8f63ceae789867b9052d4d650908839674fac
soyoon26 <barcardi26@gmail.com>
2024-06-18 22:48:04 +0900

    feat: add detailCss

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import styles from "./ProductInfo.module.css";
 export default function ProductInfo({ productDetail }) {
   return (
-    <div>
+    <div className={styles.container}>
       <div>
         <img src={productDetail.imageUrl} alt=""></img>
       </div>
-      <div>
+      <div className={styles.description}>
         <p>{productDetail.name}</p>
         <p>{productDetail.price}</p>
       </div>
