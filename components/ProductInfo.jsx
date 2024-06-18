4a5d1be8ed4bb195a9b4c250f37f726af9c3ddd4
soyoon26 <barcardi26@gmail.com>
2024-06-18 23:07:45 +0900

    feat: set Image

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./ProductInfo.module.css";
+import Image from "next/image";
 export default function ProductInfo({ productDetail }) {
   return (
     <div className={styles.container}>
       <div>
-        <img src={productDetail.imageUrl} alt=""></img>
+        <Image
+          src={productDetail.imageUrl}
+          width={250}
+          height={250}
+          alt={productDetail.name}
+        />
       </div>
       <div className={styles.description}>
         <p>{productDetail.name}</p>
         <p>{productDetail.price}</p>
+        <button>장바구니에 담기</button>
       </div>
     </div>
   );
